b2c34d0b8e6b4755997d8bfce5567f46683b995f
Gin Pham <anlongpham1405@gmail.com>
2023-07-11 17:13:27 +0700

    update package json

--- src/tests/User.controller.test.ts
+++ src/tests/User.controller.test.ts
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
 import request from 'supertest';
-import { App } from '..';
+import { App } from '../index';
 describe('App', () => {
   let app :any;
 
   before(() => {
     app = new App().app;
-
     app.listen(3001, () => {
       console.log(`Server running on port ${3001}`);
     });
   });
-  it('should respond with "Api Todo List - v1.0.0" on the root endpoint', async () => {
+  it('should respond with "Api User List - v1.0.0" on the root endpoint', async () => {
     const response = await request(app).get('/');
     expect(response.status).to.equal(200);
-    expect(response.text).to.equal('Api Todo List - v1.0.0');
+    expect(response.text).to.equal('Api User List - v1.0.0');
   });
   // Add more test cases as needed
-  it('should create a new todo', async () => {
+  it('should create a new User', async () => {
     const response = await request(app)
       .post('/api/users')
       .send({
@@ -27,11 +26,11 @@ describe('App', () => {
     });
     expect(response.status).to.equal(200);
   });
-  it('should retrieve all todos', async () => {
+  it('should retrieve all Users', async () => {
     const response = await request(app).get('/api/users');
     expect(response.status).to.equal(200);
   });
-  it('should update a todo', async () => {
+  it('should update a User', async () => {
     const createResponse = await request(app)
       .post('/api/users')
       .send({
@@ -49,7 +48,7 @@ describe('App', () => {
     });
     expect(updateResponse.status).to.equal(200);
   });
-  it('should delete a todo', async () => {
+  it('should delete a User', async () => {
     const createResponse = await request(app)
       .post('/api/users')
       .send({
