bc5c6a05155e37b0967f373868bb6c955195188b
Zocin Nguyen <nguyenvuh2r@gmail.com>
2023-07-13 15:26:31 +0700

    todo service unit test

--- src/tests/Service/Todo.service.test.ts
+++ src/tests/Service/Todo.service.test.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { starServer } from '../../app';
 import TodoService from '../../services/Todo.service';
 import { TodoInput } from '../../input/TodoInput';
+import { TodoOutput } from '../../ouput/TodoOutput';
+import { ListRespone } from "../../ouput/CommonOutput";
 
 describe('Testing Todo Service', () => {
     let app: any;
@@ -12,85 +14,96 @@ describe('Testing Todo Service', () => {
     });
 
     it('should create a new todo', async () => {
-
         const todoInput: TodoInput = {
             description : "hello",
             title : "word",
-            userId : 55
+            userId : 1,
+            isCompleted: false
         };
 
         const todo = await todoService.create(todoInput);
-        expect(todo).to.be.an('object');
-        expect(todo.description).to.equal(todoInput.description);
-        expect(todo.title).to.equal(todoInput.title);
-        expect(todo.userId).to.equal(todoInput.userId);
+        expect(todo).greaterThan(1);
+    });
 
+    it('should not create a new todo', async () => {
+        const todoInput: TodoInput = {
+            description : "",
+            title : "",
+            userId : 1,
+            isCompleted: false
+        };
+
+        const todo = await todoService.create(todoInput);
+        expect(todo).equal(0);
     });
 
     it('should get todos', async () => {
-        const rows =  await todoService.getAll();
-        expect(rows).to.be.an('array');
-    });
-
-
-    it('should get one todo', async () => {
-        // create new
         const todoInput: TodoInput = {
             description : "hello",
             title : "word",
-            userId : 55
+            userId : 1,
+            isCompleted: false
         };
 
-        const todoCreated = await todoService.create(todoInput);
-        let todoId: string = todoCreated.id;
-        // get user created
-        const todo = await todoService.getOne(todoId);
+        await todoService.create(todoInput);
 
+        const rows =  await todoService.getAll(todoInput.userId, 1, 10);
+        expect(rows.data).to.be.an('array');
+        expect(rows.data).length.greaterThan(0);
+    });
+
+    it('should get one todo', async () => {
+        const todoInput: TodoInput = {
+            description : "hello",
+            title : "word",
+            userId : 1,
+            isCompleted: false
+        };
+        const todoId = await todoService.create(todoInput);
+
+        const todo = await todoService.getOne(String(todoId));
+        expect(todo).not.null;
         expect(todo).to.be.an('object');
-        expect(todo.description).to.equal(todoCreated.description);
-        expect(todo.title).to.equal(todoCreated.title);
-        expect(todo.userId).to.equal(todoCreated.userId);
-
+        expect(todo?.description).to.equal(todoInput.description);
+        expect(todo?.title).to.equal(todoInput.title);
+        expect(todo?.isCompleted).to.equal(todoInput.isCompleted);
     });
 
     it('should update a todo', async () => {
-        // create new
         const todoInput: TodoInput = {
             description : "hello",
             title : "word",
-            userId : 55
+            userId : 1,
+            isCompleted: false
         };
-
-        const todoCreated = await todoService.create(todoInput);
-        let todoId: string = todoCreated.id;
+        const todoId = await todoService.create(todoInput);
 
         const todoUpdate: TodoInput = {
             description : "hello1",
             title : "word1",
-            userId : 55
+            userId : 1,
+            isCompleted: true
         };
 
-        await todoService.update(todoId,todoUpdate);
-        const todo = await todoService.getOne(todoId);
+        await todoService.update(String(todoId), todoUpdate);
+        const todo = await todoService.getOne(String(todoId));
         expect(todo).to.be.an('object');
-        expect(todo.description).to.equal(todoUpdate.description);
-        expect(todo.title).to.equal(todoUpdate.title);
-        expect(todo.userId).to.equal(todoUpdate.userId);
-
+        expect(todo?.description).to.equal(todoUpdate.description);
+        expect(todo?.title).to.equal(todoUpdate.title);
+        expect(todo?.isCompleted).to.equal(todoUpdate.isCompleted);
     });
 
     it('should delete one todo', async () => {
-        // create new
         const todoInput: TodoInput = {
             description : "hello",
             title : "word",
-            userId : 55
+            userId : 1,
+            isCompleted: false
         };
-        const todoCreated = await todoService.create(todoInput);
-        let todoId: string = todoCreated.id;
-        // get user created
-        await todoService.delete(todoId);
-        const todo = await todoService.getOne(todoId);
+        const todoId = await todoService.create(todoInput);
+
+        await todoService.delete(String(todoId));
+        const todo = await todoService.getOne(String(todoId));
         expect(todo).to.be.null;
     });
 
